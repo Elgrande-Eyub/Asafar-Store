8d61ddb860b666e0dca50fb3a17d311bb6fc434c
Elgrande-Eyub <ayoubfcb2002@gmail.com>
2023-12-02 12:15:03 +0100

    images updates

--- resources/views/layout/js.blade.php
+++ resources/views/layout/js.blade.php
@@ -1,4 +1,3 @@
-{{-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> --}}
 <script src="{{ asset('assets/js/jquery-1.12.4.min.js') }}"></script>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/js/chosen.min.js') }}"></script>
@@ -9,8 +8,7 @@
 <script src="{{ asset('assets/js/slick.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.zoom.min.js') }}"></script>
 <script src="{{ asset('assets/js/threesixty.min.js') }}"></script>
-<script src="assets/js/jquery-ui.min.js"></script>
+<script src="{{ asset('assets/js/jquery-ui.min.js') }}"></script>
 <script src="{{ asset('assets/js/mobilemenu.js') }}"></script>
-<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyC3nDHy1dARR-Pa_2jjPCjvsOR4bcILYsM'></script>
 <script src="{{ asset('assets/js/functions.js') }}"></script>
 <script src="{{ asset('assets/js/main.js') }}"></script>
